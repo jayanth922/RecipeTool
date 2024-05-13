7b038330264a4d20bc86e2cd51460fd520bfb0e2
Jay <jay@mymail.com>
2024-05-12 23:31:13 -0700

    All changes committed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './SignIn';
 import Home from './Home';
 import AddRecipe from './AddRecipe';
-import { getAuth, signOut } from 'firebase/auth';
+// import { getAuth, signOut } from 'firebase/auth';
 import RecipeDetails from './RecipeDetails';
 
 function App() {
